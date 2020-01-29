c325f11c09dd8d8d1ca0f2a97d88798890680738
vinodnelatur <43596674+vinodnelatur@users.noreply.github.com>
2020-01-29 13:36:27 +1100

    added a comment

--- main.go
+++ main.go
@@ -33,7 +33,7 @@ func main() {
 
 		io.WriteString(w, string(out))
 
-		fmt.Println("Hello world - the log message")
+		fmt.Println("Hello world - the log message - Commiting new changes")
 	})
 	http.ListenAndServe(":8080", nil)
 }
